9d9e8ed1958069ba5cd334366372494163ffdf72
Kumkum Bagchi <kbagchi@twilio.com>
2018-01-30 11:33:25 -0800

    Added check for 1 character long to.

--- src/server.js
+++ src/server.js
@@ -158,7 +158,12 @@ function welcome() {
 }
 
 function isNumber(to) {
-  if(String(to).charAt(0) == '+') {
+  if(to.length == 1) {
+    if(!isNaN(to)) {
+      console.log("It is a 1 digit long number" + to);
+      return true;
+    }
+  } else if(String(to).charAt(0) == '+') {
     number = to.substring(1);
     if(!isNaN(number)) {
       console.log("It is a number " + to);
